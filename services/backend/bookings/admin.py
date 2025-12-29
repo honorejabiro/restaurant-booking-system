99dc38266cd7fc7a5660715ba26933cfdcf72906
Honore Jabiro <honorejabiro@gmail.com>
2025-12-29 14:53:42 -0600

    Updated the bookings admin functionalities

--- services/backend/bookings/admin.py
+++ services/backend/bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import RestaurantTable
 
-# Register your models here.
+@admin.register(RestaurantTable)
+class RestaurantTableAdmin(admin.ModelAdmin):
+    list_display = ('table_number', 'capacity', 'is_premium')
+    list_filter = ('is_premium',)
